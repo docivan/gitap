f38b43c1ea67503ce1da5b4d49282e6942fddf5b
Dr. Ivan <i.o.potapenko@gmail.com>
2018-12-10 01:14:53 +0100

    quotes missing

--- gitap.sh
+++ gitap.sh
@@ -5,12 +5,12 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-echo "Adding file $1 to repo"
+#echo "Adding file $1 to repo"
 
-cp $1 $2
-git add $2
+cp "$1" "$2"
+git add "$2"
 git commit -m "$3"
 
-formatted_date=`date -r $1 "+%Y-%m-%d %H:%M:%S %z"`
+formatted_date=`date -r "$1" "+%Y-%m-%d %H:%M:%S %z"`
 
 GIT_COMMITTER_DATE="$formatted_date" git commit --amend --no-edit --date="$formatted_date"
